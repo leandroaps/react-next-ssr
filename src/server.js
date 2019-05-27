1193c8758ec4c12481228454ffbd5649cf8a3c9e
Leandro A. Siqueira <vrx_lasiqueira@uolinc.com>
2019-05-27 18:13:25 -0300

    P1

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import compression from "compression";
+import routes from "./routes";
+
+const app = express();
+
+app.use(compression());
+app.use(express.static("public"));
+
+app.use("/", routes);
+
+const port = process.env.PORT || 3030;
+app.listen(port, function listenHandler() {
+  console.info(`Running on ${port}...`);
+});
